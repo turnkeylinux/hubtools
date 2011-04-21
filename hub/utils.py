5d096e5a3689014fcc283777cd9456f5c281375c
Alon Swartz <alon@sterilesecurity.com>
2011-04-21 16:08:42 +0300

    created custom HubAPIError exception class (code, name, description)

--- hub/utils.py
+++ hub/utils.py
@@ -1,8 +1,15 @@
 import simplejson as json
 from pycurl_wrapper import Curl
 
-class APIError(Exception):
-    pass
+class HubAPIError(Exception):
+    def __init__(self, code, name, description):
+        Exception.__init__(self, code, name, description)
+        self.code = code
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        return "%s - %s (%s)" % (self.code, self.name, self.description)
 
 class API:
     ALL_OK = 200
@@ -21,7 +28,7 @@ class API:
 
         if not c.response_code in (cls.ALL_OK, cls.CREATED, cls.DELETED):
             name, description = c.response_data.split(":", 1)
-            raise APIError(c.response_code, name, description)
+            raise HubAPIError(c.response_code, name, description)
 
         return json.loads(c.response_data)
 
