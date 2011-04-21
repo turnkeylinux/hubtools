a51c1bdbbe30ce4239109b287e92ff3c9eb68216
Alon Swartz <alon@sterilesecurity.com>
2011-04-21 19:47:27 +0300

    formatter tweaks (private methods, white space)

--- hub/formatter.py
+++ hub/formatter.py
@@ -1,7 +1,7 @@
-def fmt_bool(bool):
+def _fmt_bool(bool):
     return "Yes" if bool else "No"
 
-def fmt_size(bytes):
+def _fmt_size(bytes):
     if bytes < (10 * 1024):
         return "0.01"
     if bytes > (1024 * 1024 * 1000 * 99.99):
@@ -9,39 +9,42 @@ def fmt_size(bytes):
     else:
         return "%-.2f" % (bytes / (1024 * 1024.0))
 
-def fmt_status(status, boot_status):
+def _fmt_status(status, boot_status):
     if status == "running" and not boot_status == "booted":
         return boot_status
     return status
 
+# backup formatters
 def fmt_backup_header():
     return "# ID  SKPP  Created     Updated     Size (MB)  Label"
 
 def fmt_backup(backup):
     return "%4s  %-3s   %s  %-10s  %-8s   %s" % \
         (backup.backup_id,
-         fmt_bool(backup.skpp),
+         _fmt_bool(backup.skpp),
          backup.created.strftime("%Y-%m-%d"),
          backup.updated.strftime("%Y-%m-%d") if backup.updated else "-",
-         fmt_size(backup.size),
+         _fmt_size(backup.size),
          backup.label)
 
+# server formatters
 def fmt_server_header():
-    return "# Status    ID         IP Address      Region          Label"
+    return "# Status      ID         IP Address      Region          Label"
 
 def fmt_server(server):
-    return "%-10s  %-9s  %-14s  %-14s  %s" % \
-        (fmt_status(server.status, server.boot_status),
-         "i-1234%s" % server.instanceid,
+    return "  %-10s  %-9s  %-14s  %-14s  %s" % \
+        (_fmt_status(server.status, server.boot_status),
+         server.instanceid,
          server.ipaddress if server.ipaddress else "-",
          server.region,
          server.label)
 
+# appliance formatters
 def fmt_appliance_header():
-    return "# Name             Ver.  Preseeds"
+    return "# Name               Ver.  Preseeds"
 
 def fmt_appliance(appliance):
-    return "%-17s  %s  %s" % \
+    return "  %-17s  %s  %s" % \
         (appliance.name,
          appliance.version.split('-')[0],
          "root_pass %s" % " ".join(appliance.preseeds))
