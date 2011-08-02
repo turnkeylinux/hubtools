fdce2d04c8c15da5641bb86e2882dcffe5b540b3
Alon Swartz <alon@sterilesecurity.com>
2011-08-02 10:51:15 +0300

    updated launch api to pass either appname or backup_id
    change:
        - the api required appname to be specified, backup_id was optional
        - if backup_id was specified, backup records were parsed to get the appname
    
        - the hub now accepts either the appname (string) or backup_id (int) to
          be specified, and will use the associated appname automatically
    
    rational:
        - simplifies api
        - moves to complexity to the hub
    
    updated hub.servers.launch() to accept appliance_name

--- hub/servers.py
+++ hub/servers.py
@@ -73,7 +73,12 @@ class Servers(object):
 
         args:
 
-            name        - appliance name (e.g., core)
+            name        - appliance|backup_id
+
+                          appliance: appliance name to launch (e.g. core)
+                          backup_id: restore backup to a new cloud server (e.g. 2)
+                                     backup key cannot be passphrase protected
+
             region      - region for instance launch (e.g., us-east-1)
             size        - instance size (e.g., m1.small)
             type        - instance type (e.g., s3 or ebs)
@@ -89,8 +94,6 @@ class Servers(object):
             fqdn        - fully qualified domain name to associate
                           e.g., www.tklapp.com. | www.example.com.
 
-            backup_id   - automatically restore backup to new cloud server
-                          note: backup key cannot be passphrase protected
         """
         attrs = {'region': region, 'size': size ,'type': type, 'label': label}
         attrs.update(kwargs)
